ddb2598d0c01db99ab8458b46a093b7b4f7deec7
Wickus Van der Merwe <vandermerwewickus@gmail.com>
2023-03-06 15:34:43 +0200

    ✨Added storage stack

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,13 +1,14 @@
-import { MyStack } from "./MyStack";
-import { App } from "@serverless-stack/resources";
+import {MyStack} from "./MyStack";
+import {App} from "@serverless-stack/resources";
+import {StorageStack} from "./StorageStack";
 
 export default function (app: App) {
-  app.setDefaultFunctionProps({
-    runtime: "nodejs16.x",
-    srcPath: "services",
-    bundle: {
-      format: "esm",
-    },
-  });
-  app.stack(MyStack);
+    app.setDefaultFunctionProps({
+        runtime: "nodejs16.x",
+        srcPath: "services",
+        bundle: {
+            format: "esm",
+        },
+    });
+    app.stack(MyStack).stack(StorageStack)
 }
